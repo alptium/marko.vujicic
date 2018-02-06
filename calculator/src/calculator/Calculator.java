3cd87cf7b3d52564d67777a822d5c5d5a45acb71
Marko <34967638+MarkoVu12@users.noreply.github.com>
2018-02-06 22:29:43 +0100

    Update Calculator.java

--- calculator/src/calculator/Calculator.java
+++ calculator/src/calculator/Calculator.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 System.out.println("Markova Matematika");
 
 
