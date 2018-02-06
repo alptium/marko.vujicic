e6c32074f1b40935c9b62c000f4d6be257b5f664
Marko <markovu12@github.com>
2018-02-06 22:37:01 +0100

    Calculator

--- calculator/src/calculator/Calculator.java
+++ calculator/src/calculator/Calculator.java
@@ -3,38 +3,38 @@ import java.util.Scanner;
 
 
 
-public class Calculator {
+	public class Calculator {
 
 	public static void main(String[] args) {
 		
-System.out.println("Markova Matematika");
+		System.out.println("Markova Matematika");
 
 
-try (Scanner sc = new Scanner(System.in)){
+		try (Scanner sc = new Scanner(System.in)){
 
 
-System.out.println ("Unesite prvi broj: ");
-int firstNumber = sc.nextInt();
-System.out.println("Izaberite operaciju: +, -, * ,/ ");
-String operation = sc.next();
-System.out.println ("Unesite drugi broj: ");
-int secondNumber = sc.nextInt();
+			System.out.println ("Unesite prvi broj: ");
+			int firstNumber = sc.nextInt();
+			System.out.println("Izaberite operaciju: +, -, * ,/ ");
+			String operation = sc.next();
+			System.out.println ("Unesite drugi broj: ");
+			int secondNumber = sc.nextInt();
 
 
-int result = 0;
+			int result = 0;
 
-if(operation.equals("+")) {
-	result = firstNumber + secondNumber;
-} else if(operation.equals("-")) {
+			if(operation.equals("+")) {
+				result = firstNumber + secondNumber;
+} 	else if(operation.equals("-")) {
 	result = firstNumber - secondNumber;
-} else if(operation.equals("*")) {
+} 	else if(operation.equals("*")) {
 	result = firstNumber * secondNumber;		
-} else if(operation.equals("/")) {
+} 	else if(operation.equals("/")) {
 	result = firstNumber / secondNumber;			
 }
 
 
-System.out.println("Rezultat je: " + result);
+	System.out.println("Rezultat je: " + result);
 
 }
 		
